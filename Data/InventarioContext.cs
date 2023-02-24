823bde2804318c96341a84c7f6442a9820dbb6bf
jimmy-20 <jasocanal@gmail.com>
2023-02-24 17:42:16 -0600

    Cambiando el idioma de español a ingles

--- Data/InventarioContext.cs
+++ Data/InventarioContext.cs
@@ -6,14 +6,14 @@ namespace Data;
 
 public class InventarioContext : DbContext
 {
-    public DbSet<Categoria> TbCategory { get; set; }
-    public DbSet<Proveedor> TbProvider { get; set; }
-    public DbSet<ProductoBase> TbBaseProduct { get; set; }
-    public DbSet<ProductoContenido> TbContentProduct { get; set; }
-    public DbSet<Producto> TbProduct { get; set; }
-    public DbSet<Cliente> TbCustomer { get; set; }
-    public DbSet<ClienteJuridico> TbLegalCustomer { get; set; }
-    public DbSet<ClienteNatural> TbNatureCustomer { get; set; }
+    public DbSet<Category> TbCategory { get; set; }
+    public DbSet<Provider> TbProvider { get; set; }
+    public DbSet<BaseProduct> TbBaseProduct { get; set; }
+    public DbSet<ContentProduct> TbContentProduct { get; set; }
+    public DbSet<Product> TbProduct { get; set; }
+    public DbSet<Customer> TbCustomer { get; set; }
+    public DbSet<LegalCustomer> TbLegalCustomer { get; set; }
+    public DbSet<NatureCustomer> TbNatureCustomer { get; set; }
 
     // public InventarioContext(DbContextOptions<InventarioContext> options) :base(options){}
 
@@ -23,69 +23,85 @@ public class InventarioContext : DbContext
         }
     }
     protected override void OnModelCreating(ModelBuilder builder){
-        //Datos semillas
-        builder.Entity<Categoria>().HasData(InitCategoria());
-        builder.Entity<Proveedor>().HasData(InitProveedor());
-        builder.Entity<ProductoBase>().HasData(InitProductoBase());
-        builder.Entity<ProductoContenido>().HasData(InitProductContent());
-        builder.Entity<Producto>().HasData(InitProducts());
+        #region configuring_data_type
+        //Cambiando los nvarchar a varchar(Configuracion masiva)
+        foreach (var property in builder.Model.GetEntityTypes().SelectMany(entity => entity.GetDeclaredProperties().Where(property => property.ClrType == typeof(string))))
+        {
+            property.SetColumnType("varchar");
+        }
+        #endregion
+
+        #region restrictions
+        builder.Entity<Product>().Property(p => p.UrlImage).HasColumnType("nvarchar").HasMaxLength(1000);
+        builder.Entity<NatureCustomer>().Property(p => p.Identification).HasColumnType("char");
+        builder.Entity<LegalCustomer>().Property(p => p.Ruc).HasColumnType("char");
+        builder.Entity<Provider>().Property(p => p.Ruc).HasColumnType("char");
+        #endregion
+
+        #region Seed_Data
+        builder.Entity<Category>().HasData(InitCategory());
+        builder.Entity<Provider>().HasData(InitProvider());
+        builder.Entity<BaseProduct>().HasData(InitBaseProduct());
+        builder.Entity<ContentProduct>().HasData(InitProductContent());
+        builder.Entity<Product>().HasData(InitProducts());
+        #endregion
     }
 
-    private IEnumerable<Categoria> InitCategoria(){
-        List<Categoria> categories = new List<Categoria>(){
-            new (){IdCategoria=1,Nombre="Lacteos",Descripcion="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's"},
-            new (){IdCategoria=2, Nombre = "Bebidas carbonatadas",Descripcion="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's"},
-            new (){IdCategoria=3, Nombre="Aseo personal", Descripcion="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's"},
-            new (){IdCategoria=4, Nombre="Aseo del hogar",Descripcion="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's"},
-            new (){IdCategoria=5, Nombre="Snacks", Descripcion="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's"},
-            new (){IdCategoria=6, Nombre="Proteinas", Descripcion="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's"}
+    private IEnumerable<Category> InitCategory(){
+        List<Category> categories = new List<Category>(){
+            new (){IdCategory=1,Name="Lacteos",Description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's"},
+            new (){IdCategory=2, Name = "Bebidas carbonatadas",Description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's"},
+            new (){IdCategory=3, Name="Aseo personal", Description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's"},
+            new (){IdCategory=4, Name="Aseo del hogar",Description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's"},
+            new (){IdCategory=5, Name="Snacks", Description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's"},
+            new (){IdCategory=6, Name="Proteinas", Description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's"}
         };
 
         return categories;
     }
 
-    private IEnumerable<Proveedor> InitProveedor(){
-        List<Proveedor> providers = new List<Proveedor>(){
-            new(){IdProveedor=1, Nombre="Coca Cola Company",Telefono="22113355",Direccion="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's"},
-            new(){IdProveedor=2, Nombre="Pepsi",Telefono="88990123",Direccion="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's"},
-            new(){IdProveedor=3, Nombre="Dell",Telefono="78901010",Direccion="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's"}
+    private IEnumerable<Provider> InitProvider(){
+        List<Provider> providers = new List<Provider>(){
+            new(){IdProvider=1, Name="Coca Cola Company",Ruc="J0312010201321",Phone="22113355",Address="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's"},
+            new(){IdProvider=2, Name="Pepsi",Ruc="J0312010201322",Phone="88990123",Address="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's"},
+            new(){IdProvider=3, Name="Dell",Ruc="J0312010201323",Phone="78901010",Address="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's"}
         };
 
         return providers;
     }
 
-    private IEnumerable<ProductoBase> InitProductoBase(){
-        List<ProductoBase> productsBase = new List<ProductoBase>(){
-            new(){IdBase = 1,IdCategoria=2,IdProveedor=1,Nombre="Coca cola"},
-            new(){IdBase=2,IdCategoria=2, IdProveedor=1,Nombre="Fanta roja"},
-            new(){IdBase=3,IdCategoria=2, IdProveedor=1,Nombre="Fanta naranja"},
-            new(){IdBase=4,IdCategoria=2, IdProveedor=2,Nombre="Pepsi"},
-            new(){IdBase=5,IdCategoria=2, IdProveedor=2,Nombre="Rojita"},
-            new(){IdBase=6,IdCategoria=2, IdProveedor=3,Nombre="Latitude E7440"}
+    private IEnumerable<BaseProduct> InitBaseProduct(){
+        List<BaseProduct> productsBase = new List<BaseProduct>(){
+            new(){IdBase = 1,IdCategory=2,IdProvider=1,Name="Coca cola"},
+            new(){IdBase=2,IdCategory=2, IdProvider=1,Name="Fanta roja"},
+            new(){IdBase=3,IdCategory=2, IdProvider=1,Name="Fanta naranja"},
+            new(){IdBase=4,IdCategory=2, IdProvider=2,Name="Pepsi"},
+            new(){IdBase=5,IdCategory=2, IdProvider=2,Name="Rojita"},
+            new(){IdBase=6,IdCategory=2, IdProvider=3,Name="Latitude E7440"}
         };
 
         return productsBase;
     }
 
-    private IEnumerable<ProductoContenido> InitProductContent(){
-        List<ProductoContenido> productsContents = new List<ProductoContenido>(){
-            new(){IdContenido=1,Modelo="Articulo",Contenido="Unidad"},
-            new(){IdContenido=2,Modelo="Retornable",Contenido="Litro"},
-            new(){IdContenido=3,Modelo="Lata",Contenido="Mililitro"},
-            new(){IdContenido=4,Modelo="Desechable",Contenido="Mililitro"},
-            new(){IdContenido=5, Modelo="Desechable",Contenido="Litro"},
-            new(){IdContenido=6, Modelo="Vidrio",Contenido="Mililitro"},
-            new(){IdContenido=7,Modelo="Vidrio",Contenido="Litro"}
+    private IEnumerable<ContentProduct> InitProductContent(){
+        List<ContentProduct> productsContents = new List<ContentProduct>(){
+            new(){IdContent=1,Model="Articulo",Content="Unidad"},
+            new(){IdContent=2,Model="Retornable",Content="Litro"},
+            new(){IdContent=3,Model="Lata",Content="Mililitro"},
+            new(){IdContent=4,Model="Desechable",Content="Mililitro"},
+            new(){IdContent=5, Model="Desechable",Content="Litro"},
+            new(){IdContent=6, Model="Vidrio",Content="Mililitro"},
+            new(){IdContent=7,Model="Vidrio",Content="Litro"}
         };
 
         return productsContents;
     }
 
-    private IEnumerable<Producto> InitProducts(){
-        List<Producto> products = new List<Producto>(){
-            new(){IdProducto=1,IdBase=1,IdContainer=2,Medida=2,Precio=38,UrlImagen="https://holymarket.cl/wp-content/uploads/2022/03/7801610001295-1.png",Existencia=10,Estado=EstadoCatalogo.Registrado},
-            new(){IdProducto=2,IdBase=2,IdContainer=2,Medida=2,Precio=39,UrlImagen="https://docplayer.es/docs-images/109/187608361/images/12-3.jpg",Existencia=5,Estado=EstadoCatalogo.Registrado},
-            new(){IdProducto=3,IdBase=3,IdContainer=2,Medida=2,Precio=39,UrlImagen="http://d2r9epyceweg5n.cloudfront.net/stores/001/160/839/products/005-004-011_fanta-retornable-x-2-lts1-67b8aab041082aa14315884684037152-640-0.jpg",Existencia=13,Estado=EstadoCatalogo.Registrado}
+    private IEnumerable<Product> InitProducts(){
+        List<Product> products = new List<Product>(){
+            new(){IdProduct=1,IdBase=1,IdContent=2,Volume=2,Price=38,UrlImage="https://holymarket.cl/wp-content/uploads/2022/03/7801610001295-1.png",Quantity=10,State=CatalogState.Register},
+            new(){IdProduct=2,IdBase=2,IdContent=2,Volume=2,Price=39,UrlImage="https://docplayer.es/docs-images/109/187608361/images/12-3.jpg",Quantity=5,State=CatalogState.Register},
+            new(){IdProduct=3,IdBase=3,IdContent=2,Volume=2,Price=39,UrlImage="http://d2r9epyceweg5n.cloudfront.net/stores/001/160/839/products/005-004-011_fanta-retornable-x-2-lts1-67b8aab041082aa14315884684037152-640-0.jpg",Quantity=13,State=CatalogState.Register}
         };
 
         return products;
---
+++ Data/Migrations/20230224232950_Init Base Model.cs
@@ -0,0 +1,284 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitBaseModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CategoryProduct",
+                columns: table => new
+                {
+                    IdCategory = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "varchar(300)", maxLength: 300, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CategoryProduct", x => x.IdCategory);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ContentProduct",
+                columns: table => new
+                {
+                    IdContent = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Model = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: false),
+                    Content = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContentProduct", x => x.IdContent);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customer",
+                columns: table => new
+                {
+                    IdCustomer = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Phone = table.Column<string>(type: "varchar(8)", maxLength: 8, nullable: false),
+                    Address = table.Column<string>(type: "varchar(300)", maxLength: 300, nullable: false),
+                    Email = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customer", x => x.IdCustomer);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Provider",
+                columns: table => new
+                {
+                    IdProvider = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false),
+                    Ruc = table.Column<string>(type: "char(14)", maxLength: 14, nullable: false),
+                    Phone = table.Column<string>(type: "varchar(8)", maxLength: 8, nullable: false),
+                    Address = table.Column<string>(type: "varchar(150)", maxLength: 150, nullable: false),
+                    Email = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: true),
+                    State = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Provider", x => x.IdProvider);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LegalCustomer",
+                columns: table => new
+                {
+                    IdCustomer = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    Ruc = table.Column<string>(type: "char(14)", maxLength: 14, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LegalCustomer", x => x.IdCustomer);
+                    table.ForeignKey(
+                        name: "FK_LegalCustomer_Customer_IdCustomer",
+                        column: x => x.IdCustomer,
+                        principalTable: "Customer",
+                        principalColumn: "IdCustomer",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NatureCustomer",
+                columns: table => new
+                {
+                    IdCustomer = table.Column<int>(type: "int", nullable: false),
+                    FirstName = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false),
+                    SecondName = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false),
+                    FirstLastName = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false),
+                    SecondLastName = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false),
+                    Identification = table.Column<string>(type: "char(14)", maxLength: 14, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NatureCustomer", x => x.IdCustomer);
+                    table.ForeignKey(
+                        name: "FK_NatureCustomer_Customer_IdCustomer",
+                        column: x => x.IdCustomer,
+                        principalTable: "Customer",
+                        principalColumn: "IdCustomer",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BaseProduct",
+                columns: table => new
+                {
+                    IdBase = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdCategory = table.Column<int>(type: "int", nullable: false),
+                    IdProvider = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BaseProduct", x => x.IdBase);
+                    table.ForeignKey(
+                        name: "FK_BaseProduct_CategoryProduct_IdCategory",
+                        column: x => x.IdCategory,
+                        principalTable: "CategoryProduct",
+                        principalColumn: "IdCategory",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BaseProduct_Provider_IdProvider",
+                        column: x => x.IdProvider,
+                        principalTable: "Provider",
+                        principalColumn: "IdProvider",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    IdProduct = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdBase = table.Column<int>(type: "int", nullable: false),
+                    IdContent = table.Column<int>(type: "int", nullable: false),
+                    Volume = table.Column<double>(type: "float", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    UrlImage = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    State = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.IdProduct);
+                    table.ForeignKey(
+                        name: "FK_Product_BaseProduct_IdBase",
+                        column: x => x.IdBase,
+                        principalTable: "BaseProduct",
+                        principalColumn: "IdBase",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Product_ContentProduct_IdContent",
+                        column: x => x.IdContent,
+                        principalTable: "ContentProduct",
+                        principalColumn: "IdContent",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "CategoryProduct",
+                columns: new[] { "IdCategory", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's", "Lacteos" },
+                    { 2, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's", "Bebidas carbonatadas" },
+                    { 3, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's", "Aseo personal" },
+                    { 4, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's", "Aseo del hogar" },
+                    { 5, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's", "Snacks" },
+                    { 6, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's", "Proteinas" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ContentProduct",
+                columns: new[] { "IdContent", "Content", "Model" },
+                values: new object[,]
+                {
+                    { 1, "Unidad", "Articulo" },
+                    { 2, "Litro", "Retornable" },
+                    { 3, "Mililitro", "Lata" },
+                    { 4, "Mililitro", "Desechable" },
+                    { 5, "Litro", "Desechable" },
+                    { 6, "Mililitro", "Vidrio" },
+                    { 7, "Litro", "Vidrio" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Provider",
+                columns: new[] { "IdProvider", "Address", "Email", "Name", "Phone", "Ruc", "State" },
+                values: new object[,]
+                {
+                    { 1, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's", null, "Coca Cola Company", "22113355", "J0312010201321", 1 },
+                    { 2, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's", null, "Pepsi", "88990123", "J0312010201322", 1 },
+                    { 3, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's", null, "Dell", "78901010", "J0312010201323", 1 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "BaseProduct",
+                columns: new[] { "IdBase", "IdCategory", "IdProvider", "Name" },
+                values: new object[,]
+                {
+                    { 1, 2, 1, "Coca cola" },
+                    { 2, 2, 1, "Fanta roja" },
+                    { 3, 2, 1, "Fanta naranja" },
+                    { 4, 2, 2, "Pepsi" },
+                    { 5, 2, 2, "Rojita" },
+                    { 6, 2, 3, "Latitude E7440" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Product",
+                columns: new[] { "IdProduct", "IdBase", "IdContent", "Price", "Quantity", "State", "UrlImage", "Volume" },
+                values: new object[,]
+                {
+                    { 1, 1, 2, 38.0, 10, 1, "https://holymarket.cl/wp-content/uploads/2022/03/7801610001295-1.png", 2.0 },
+                    { 2, 2, 2, 39.0, 5, 1, "https://docplayer.es/docs-images/109/187608361/images/12-3.jpg", 2.0 },
+                    { 3, 3, 2, 39.0, 13, 1, "http://d2r9epyceweg5n.cloudfront.net/stores/001/160/839/products/005-004-011_fanta-retornable-x-2-lts1-67b8aab041082aa14315884684037152-640-0.jpg", 2.0 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BaseProduct_IdCategory",
+                table: "BaseProduct",
+                column: "IdCategory");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BaseProduct_IdProvider",
+                table: "BaseProduct",
+                column: "IdProvider");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_IdBase",
+                table: "Product",
+                column: "IdBase");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_IdContent",
+                table: "Product",
+                column: "IdContent");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LegalCustomer");
+
+            migrationBuilder.DropTable(
+                name: "NatureCustomer");
+
+            migrationBuilder.DropTable(
+                name: "Product");
+
+            migrationBuilder.DropTable(
+                name: "Customer");
+
+            migrationBuilder.DropTable(
+                name: "BaseProduct");
+
+            migrationBuilder.DropTable(
+                name: "ContentProduct");
+
+            migrationBuilder.DropTable(
+                name: "CategoryProduct");
+
+            migrationBuilder.DropTable(
+                name: "Provider");
+        }
+    }
+}
